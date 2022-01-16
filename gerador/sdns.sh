b65efcb05b681a8f7c94ed3d614772e7f45302e0
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-01-16 08:32:30 -0500

    Update sdns.sh

--- gerador/sdns.sh
+++ gerador/sdns.sh
@@ -79,7 +79,7 @@ echo -e "\033[1;31m════════════════════�
 echo ""
 echo -e "          \033[1;33mINSTALACION FINALIZADA!\033[0m          "
 echo ""
-echo -e "Para abrir el menu, use el comando: \033[1;33mslowdns\033[0m"
+echo -e "Para abrir otravez el menu, use el comando: \033[1;33mslowdns\033[0m"
 cd
 #rm install
 slowdns
