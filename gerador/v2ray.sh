9fe9b138d9de46c2d962b37768d6fb7962bbb6f2
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-02-07 14:04:44 -0500

    Update v2ray.sh

--- gerador/v2ray.sh
+++ gerador/v2ray.sh
@@ -186,7 +186,7 @@ usris="$(cat /etc/RegV2ray|grep -w "$hostreturn"|cut -d'|' -f2)"
 #local contador_secuencial+="\e[93m$hostreturn \e[97m|\e[93m$usris\e[97m|\e[93m $EXPTIME \n"
 #local contador_secuencial+="\e[93m$usris\e[97m"
 #local contador_secuencial+="\e[93m $EXPTIME \n"
-local contador_secuencial+="\e[93mUUID: $hostreturn \e[97m| \n \e[93mUSUARIO: $usris\e[97m| \n \e[93mEXPIRACION: $EXPTIME \n"
+local contador_secuencial+="\e[92mUUID:\e[93m $hostreturn \e[97m| \n \e[92mUSUARIO:\e[93m $usris \n \e[92mEXPIRACION:\e[97m $EXPTIME \n"
       if [[ $i -gt 30 ]]; then
 
 	      echo -e "$contador_secuencial"
