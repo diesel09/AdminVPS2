02b1305d6c4aa19947e767035a04b3d3b289ffde
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-02-07 08:58:01 -0500

    Update ssl.sh

--- gerador/ssl.sh
+++ gerador/ssl.sh
@@ -190,6 +190,179 @@ msg -verm "Procedimiento Cancelado: "
 msg -bar
 fi
 }
+certif(){
+ [[ $(mportas|grep stunnel4|head -1) ]] && {
+ echo -e "\\033[1;33m $(fun_trans  "Deteniendo Stunnel")"
+ msg -bar
+ service stunnel4 stop > /dev/null 2>&1
+ apt-get purge stunnel4 -y &>/dev/null && echo -e "\\e[31m DETENIENDO SERVICIO SSL" | pv -qL10
+ apt-get remove stunnel4 &>/dev/null
+ rm -rf /etc/stunnel/stunnel.conf
+ rm -rf /etc/stunnel/private.key
+ rm -rf /etc/stunnel/certificate.crt
+ rm -rf /etc/stunnel/ca_bundle.crt
+ msg -bar
+ echo -e "\\033[1;33m $(fun_trans  "Detenido Con Exito!")"
+ msg -bar
+ return 0
+ }
+ clear
+ msg -bar
+ echo -e "\\033[1;33m $(fun_trans  "Seleccione una puerta de redirección interna.")"
+ echo -e "\\033[1;33m $(fun_trans  "Un puerto SSH/DROPBEAR/SQUID/OPENVPN/PYTHON")"
+ msg -bar
+          while true; do
+          echo -ne "\\033[1;37m"
+          read -p " Puerto Local: " redir
+ 		 echo ""
+          if [[ ! -z $redir ]]; then
+              if [[ $(echo $redir|grep [0-9]) ]]; then
+                 [[ $(mportas|grep $redir|head -1) ]] && break || echo -e "\\033[1;31m $(fun_trans  "Puerto Invalido")"
+              fi
+          fi
+          done
+ msg -bar
+ DPORT="$(mportas|grep $redir|awk '{print $2}'|head -1)"
+ echo -e "\\033[1;33m $(fun_trans  "Ahora Que Puerto sera SSL")"
+ msg -bar
+     while true; do
+ 	echo -ne "\\033[1;37m"
+     read -p " Puerto SSL: " SSLPORT
+ 	echo ""
+     [[ $(mportas|grep -w "$SSLPORT") ]] || break
+     echo -e "\\033[1;33m $(fun_trans  "Esta puerta está en uso")"
+     unset SSLPORT
+     done
+ msg -bar
+ echo -e "\\033[1;33m $(fun_trans  "Instalando SSL")"
+ msg -bar
+ apt-get install stunnel4 -y &>/dev/null && echo -e "\\e[32m INSTALANDO SSL" | pv -qL10
+ clear
+ echo -e "client = no\\n[SSL]\\ncert = /etc/stunnel/stunnel.pem\\naccept = ${SSLPORT}\\nconnect = 127.0.0.1:${DPORT}" > /etc/stunnel/stunnel.conf
+ msg -bar
+ echo -e "\\e[1;37m ACONTINUACION ES TENER LISTO EL LINK DEL CERTIFICADO.zip\\n VERIFICAR CERTIFICADO EN ZEROSSL, DESCARGALO Y SUBELO\\n EN TU GITHUB O DROPBOX"
+ msg -bar
+ read -p "enter para seguir..."
+ clear
+ ####Cerrificado ssl/tls#####
+ msg -bar
+ echo -e "\\e[1;33m👇 LINK DEL CERTIFICADO.zip 👇           \\n \\e[0m"
+ echo -e "\\e[1;36m LINK \\e[37m: \\e[34m\\c "
+ #extraer certificado.zip
+ read linkd
+ wget $linkd &>/dev/null -O /etc/stunnel/certificado.zip
+ cd /etc/stunnel/
+ unzip certificado.zip &>/dev/null
+ cat private.key certificate.crt ca_bundle.crt > stunnel.pem
+ rm -rf certificado.zip
+ sed -i 's/ENABLED=0/ENABLED=1/g' /etc/default/stunnel4
+ service stunnel restart > /dev/null 2>&1
+ service stunnel4 restart &>/dev/null
+ msg -bar
+ echo -e "${cor[4]} CERTIFICADO INSTALADO CON EXITO \\e[0m" 
+ msg -bar
+ }
+ 
+ certificadom(){
+ if [ -f /etc/stunnel/stunnel.conf ]; then
+ insapa2(){
+ for pid in $(pgrep python);do
+ kill $pid
+ done
+ for pid in $(pgrep apache2);do
+ kill $pid
+ done
+ service dropbear stop
+ apt install apache2 -y
+ echo "Listen 80
+ 
+ <IfModule ssl_module>
+         Listen 443
+ </IfModule>
+ 
+ <IfModule mod_gnutls.c>
+         Listen 443
+ </IfModule> " > /etc/apache2/ports.conf
+ service apache2 restart
+ }
+ clear
+ msg -bar
+ insapa2 &>/dev/null && echo -e " \\e[1;33mAGREGANDO RECURSOS " | pv -qL 10
+ msg -bar
+ echo -e "\\e[1;37m Verificar dominio \\e[0m"
+ msg -bar
+ read -p " LLAVE: " keyy
+ msg -bar
+ read -p " DATOS: " dat2w
+ mkdir -p /var/www/html/.well-known/pki-validation/
+ datfr1=$(echo "$dat2w"|awk '{print $1}')
+ datfr2=$(echo "$dat2w"|awk '{print $2}')
+ datfr3=$(echo "$dat2w"|awk '{print $3}')
+ echo -ne "${datfr1}\\n${datfr2}\\n${datfr3}" >/var/www/html/.well-known/pki-validation/$keyy.txt
+ msg -bar
+ echo -e "\\e[1;37m VERIFIQUE EN LA PÁGINA ZEROSSL \\e[0m"
+ msg -bar
+ read -p " ENTER PARA CONTINUAR"
+ clear
+ msg -bar
+ echo -e "\\e[1;33m👇 LINK DEL CERTIFICADO 👇\\n \\e[0m"
+ echo -e "\\e[1;36m LINK \\e[37m: \\e[34m\\c"
+ read link
+ incertis(){
+ wget $link -O /etc/stunnel/certificado.zip
+ cd /etc/stunnel/
+ unzip certificado.zip 
+ cat private.key certificate.crt ca_bundle.crt > stunnel.pem
+ service stunnel restart &>/dev/null
+ service stunnel4 restart &>/dev/null
+ }
+ incertis &>/dev/null && echo -e " \\e[1;33mEXTRAYENDO CERTIFICADO " | pv -qL 10
+ msg -bar
+ echo -e "${cor[4]} CERTIFICADO INSTALADO \\e[0m" 
+ msg -bar
+ 
+ for pid in $(pgrep apache2);do
+ kill $pid
+ done
+ apt install apache2 -y &>/dev/null
+ echo "Listen 81
+ 
+ <IfModule ssl_module>
+         Listen 443
+ </IfModule>
+ 
+ <IfModule mod_gnutls.c>
+         Listen 443
+ </IfModule> " > /etc/apache2/ports.conf
+ service apache2 restart &>/dev/null
+ service dropbear start &>/dev/null
+ service dropbear restart &>/dev/null
+ for port in $(cat /etc/VPS-MX/PortPD.log| grep -v "nobody" |cut -d' ' -f1)
+ do
+ PIDVRF3="$(ps aux|grep pydic-"$port" |grep -v grep|awk '{print $2}')"
+ if [[ -z $PIDVRF3 ]]; then
+ screen -dmS pydic-"$port" python /etc/VPS-MX/protocolos/PDirect.py "$port"
+ else
+ for pid in $(echo $PIDVRF3); do
+ echo ""
+ done
+ fi
+ done
+ else
+ msg -bar
+ echo -e "${cor[3]} SSL/TLS NO INSTALADO \\e[0m"
+ msg -bar
+ fi
+ }
+ 
+ 
+ clear
+ if netstat -tnlp |grep 'stunnel4' &>/dev/null; then
+ stunel="\\e[32m[ ON ]"
+ else
+ stunel="\\e[31m[ OFF ]"
+ fi
+
 clear
 msg -bar
 echo -e "${cor[3]}       INSTALADOR MONO Y MULTI SSL"
@@ -199,6 +372,7 @@ msg -bar
 echo "1).- INSTALAR | DETENER SSL "
 echo "2).- AGREGAR OTRO PUERTO SSL   "
 echo "3).- WEBSOCKET (AUTO CONFIGURACION)   "
+echo "4).- CERTIFICADO SSL/TLS   "
 msg -bar && echo -ne "$(msg -verd "[0]") $(msg -verm2 ">") "&& msg -bra "\033[1;41mREGRESAR AL MENU"
 msg -bar
 echo -n "Seleccione una opcion: "
@@ -219,5 +393,26 @@ msg -bar
 ssl_py
 sleep 3
 exit
+;;
+4)
+ clear
+ msg -bar
+ msg -ama "	CERTIFICADO"
+ msg -bar
+ echo -e "${cor[4]} 1).-\\033[1;37m CERTIFICADO WEB ZIP "
+ echo -e "${cor[4]} 2).-\\033[1;37m CERTIFICADO MANUAL ZEROSSL   "
+ msg -bar
+ echo -ne "\\033[1;37mSELECCIONE UN NÚMERO [0/2]: "
+ read opc
+ case $opc in
+ 1)
+ certif
+ exit
+ ;;
+ 2)
+ certificadom
+ exit
+ ;;
+ esac
 ;;
 esac
