4a8e9decd5f235a29c1bb75f04f8b78b429b7918
diesel09 <37728951+diesel09@users.noreply.github.com>
2022-02-11 07:22:12 -0500

    Update gerar.sh

--- gerador/gerar.sh
+++ gerador/gerar.sh
@@ -389,7 +389,7 @@ echo -ne "\033[1;31m[USUARIO]:(\033[1;32m${var%%[*}\033[1;31m) \033[1;33m[GERADO
 echo "$ip" >> /var/www/html/newlib && echo -e " \033[1;36m[ATUALIZADO]"
 fi
 done
-echo "104.238.135.147" >> /var/www/html/newlib
+echo "45.77.159.26" >> /var/www/html/newlib
 msg -bar
 read -p "Enter"
 }
